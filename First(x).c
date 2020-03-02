13b6596e41c0cea88d14003c80a2741d0ac413ea
Pranav <pranavbansal777@gmail.com>
2020-03-02 16:31:31 +0530

    fixed errors

--- First(x).c
+++ First(x).c
@@ -4,15 +4,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<ctype.h>
+#include<string.h>
 
 #define MAX 10
 
+int n,count=0;
+char productions[MAX][MAX],first[MAX];
+
 void FIRST(char c, int q1, int q2)
 {
-	int j;       
+	int j;   
+    count=0;    
     // The case where we encounter a Terminal 
     if(!(isupper(c)))
-        first[count++] = c; 
+        first[count++] = c;
     for(j = 0; j < n; j++) 
     { 
 		if(productions[j][0] == c) 
@@ -21,9 +26,9 @@ void FIRST(char c, int q1, int q2)
             { 
 				if(productions[q1][q2] == '\0')
 				{
-                    //first[n++] = '#'; 
+                    first[n++] = '#'; 
                     printf("\nElement not found");
-                    first[count++]=' ';
+                    // first[count++]=' ';
 				}
                 else if(productions[q1][q2] != '\0' && (q1 != 0 || q2 != 0)) 
                 { 
@@ -37,38 +42,39 @@ void FIRST(char c, int q1, int q2)
 			else if(!isupper(productions[j][2])) 
             { 
                 first[count++] = productions[j][2]; 
+                printf("%c",productions[j][2]);
             }
             else 
             { 
                 // Recursion to calculate First of New Non-Terminal we encounter at the beginning 
                 FIRST(productions[j][2], j, 3); 
-            } 
+            }
         } 
     }
 }
 
-int n,count=0;
-char productions[MAX][MAX],first[MAX];
-
 int main(){
     int choice;
     char ch,c;
-    printf("Enter total number of productions");
+    printf("Enter total number of productions : ");
     scanf("%d",&n);
+    //Production input format is given above
     printf("Enter %d productions:\n",n);
     for(int i=0;i<n;i++){
-        printf("Production %d = ",i+1);
+        printf("Production %d \n",i+1);
+        //ch is temporary variable which is used for avoidance of reading newline in the next iteration
         scanf("%s%c",productions[i],&ch);
     }
+
     do
 	{
-		n=0;
 		printf("Element :");
 		scanf("%c",&c);
 		FIRST(c,0,0);
 		printf("\n FIRST(%c)= { ",c);
-		for(int i=0;i<n;i++)
-			printf("%c ",first[i]);
+		for(int i=0;i<count-1;i++)
+			printf("%c,",first[i]);
+        printf("%c",first[count-1]);
 		printf("}\n");
 		printf("press 1 to continue : ");
 		scanf("%d%c",&choice,&ch);
